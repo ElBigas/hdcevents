9159a3e6f9981ae4467772f62d89cc5da0460e82
ElBigas <elbigas@protonmail.com>
2023-12-13 08:50:58 -0300

    feat: Adicionado link para Google Fonts e Bootstrap

--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -1,11 +1,14 @@
+{{-- Aqui é definido o layout principal --}}
 @extends('layouts.main')
 
-@section('title', 'Contato')
+{{-- tiulo da pagina --}}
+@section('title', 'Home')
 
+{{-- conteudo da pagina --}}
 @section('content')
 
-<h1>pagina de contato</h1>
+    <h1>pagina de contato</h1>
 
-<a href="/">voltar para home</a>
+    <a href="/">voltar para home</a>
 
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,10 @@
+{{-- Aqui é definido o layout principal --}}
 @extends('layouts.main')
 
+{{-- tiulo da pagina --}}
 @section('title', 'Home')
 
+{{-- conteudo da pagina --}}
 @section('content')
 
     <h1>Bem vindo ao HDC Events</h1>
