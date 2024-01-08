fded0c06b8ddccb776e98c134959d8dab5778465
ElBigas <elbigas@protonmail.com>
2024-01-08 17:48:05 -0300

    feat(views): atualiza view welcome
    Adiciona campo de busca na view welcome para facilitar a pesquisa de eventos.
    Inclui também um bloco para exibir os próximos eventos, melhorando a experiência
    do usuário ao navegar na página inicial.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,31 @@
 {{-- conteudo da pagina --}}
 @section('content')
 
-    <h1>Bem vindo ao HDC Events!</h1>
+    <div id="search-container" class="col-md-12 pb-5">
+        <h1>Busque um evento</h1>
+        <form action="">
+            <input type="text" name="search" id="search" class="form-control" placeholder="Procurar...">
+        </form>
+    </div>
 
-    @foreach ($events as $event)
-        <p>{{ $event->title }} - {{ $event->description }} </p>
-    @endforeach
+    <div id="events-container" class="col-md-12">
+        <h2>Próximos Eventos</h2>
+        <p>Veja os próximos eventos</p>
+
+        <div id="cards-container" class="d-flex flex-wrap gap-3">
+            @foreach ($events as $event)
+                <div class="card col-md-3 ">
+                    <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+
+                    <div class="card-body">
+                        <p class="card-date">05/12/2024</p>
+                        <h5 class="card-title">{{ $event->title }}</h5>
+                        <p class="card-participants">100 participantes</p>
+                        <a href="#" class="btn btn-primary">Saber mais</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 
 @endsection
