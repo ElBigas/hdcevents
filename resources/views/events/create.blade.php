b5ff065077d0578776190e2b7df81a1222cce6c2
ElBigas <elbigas@protonmail.com>
2024-01-09 11:32:21 -0300

    feat(events): adiciona formulário 'create' para criação de eventos
    Cria um formulário 'create' em que os usuários podem inserir todos os dados necessários para a criação de um evento. O formulário envia os dados para a rota '/events' por meio de um método POST. Além disso, adiciona a rota POST no 'EventController' e cria uma ação para tratar e armazenar os dados do formulário no banco de dados.

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -3,5 +3,42 @@
 @section('title', 'Criar Evento')
 
 @section('content')
-    <h1>Criar evento</h1>
+
+    <div id="event-create-container" class="col-md-6 offset-md-3">
+
+        <h1>Criar evento</h1>
+
+        <form action="/events" method="POST">
+            @csrf
+            <div class="form-group">
+                <div class="mb-3">
+                    <label class="form-label" for="title">Evento:</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento"
+                        required>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label" for="city">Cidade:</label>
+                    <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento"
+                        required>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label" for="private">O evento é privado?</label>
+                    <select name="private" id="private" class="form-select" required>
+                        <option value="0">Não</option>
+                        <option value="1">Sim</option>
+                    </select>
+                </div>
+
+                <div class="mb-3">
+                    <label class="form-label" for="description">Descrição:</label>
+                    <textarea rows="3" class="form-control" id="description" name="description" placeholder="Descrição do evento" required></textarea>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+
+    </div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EventController::class, 'index']);
-
 Route::get('/events/create ', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/contato', function () {
     return view('contato');
